748dedf7316d06d2797034b0ac5914becf185bac
Paweł Data <123pawel99@gmail.com>
2019-10-31 20:31:18 +0100

    Create Database class, fixed variable in Product

--- src/main/java/invoice/Product.java
+++ src/main/java/invoice/Product.java
@@ -2,10 +2,18 @@ package invoice;
 
 public class Product {
 	private String name;
-	private String recommendedPrice;
+	private Float recommendedPrice;
 	
-	public Product(String name, String recommendedPrice) {
+	public Product(String name, Float recommendedPrice) {
 		this.name = name;
 		this.recommendedPrice = recommendedPrice;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Float getRecPrice() {
+		return this.recommendedPrice;
 	}
 }
